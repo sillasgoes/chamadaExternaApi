cc8eee1ca7dd5acb3fc347a5784d6c1ef9963bca
Sillas <sillasgoes@gmail.com>
2025-11-10 08:55:04 -0300

    First Commit

---
+++ src/main/java/com/sillas/Chamadas/para/Api/Externa/ChamadasParaApiExternaApplication.java
@@ -0,0 +1,13 @@
+package com.sillas.Chamadas.para.Api.Externa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamadasParaApiExternaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamadasParaApiExternaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sillas/Chamadas/para/Api/Externa/ChamadasParaApiExternaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sillas.Chamadas.para.Api.Externa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamadasParaApiExternaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
